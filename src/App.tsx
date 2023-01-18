820f2a12288ab75561c4f8d2f8a88858dd9a01af
sniffjoke <41764936+sniffjoke@users.noreply.github.com>
2023-01-18 19:21:32 +0100

    check redux with counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
+import {userSlice} from "./store/reducers/UserSlice";
 
 function App() {
+    const {count} = useAppSelector(state => state.userReducer)
+    const {increment} = userSlice.actions
+    const dispacth = useAppDispatch()
+
   return (
     <div className="App">
-
+      <h1>{count}</h1>
+        <button onClick={() => dispacth(increment(1))}>
+            INCREMENT
+        </button>
     </div>
   );
 }
